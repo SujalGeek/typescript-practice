0c0cd93ec96643c6c41cde01a8eb3adb7c50dc92
SujalGeek <morwanisujal875@gmail.com>
2025-01-15 21:35:40 +0530

    basic understanding of array

--- arrays/array.ts
+++ arrays/array.ts
@@ -1,1 +1,31 @@
 // Array
+const superheros: string[] = []
+const heropower: number[] =[]
+
+superheros.push("thor")
+superheros.push("ironman")
+
+heropower.push(2);
+heropower.push(3)
+
+// Another way
+const heropower2: Array<number>=[]
+heropower2.push(4);
+heropower2.push(5);
+
+type User4 = {
+  name:string;
+  isActive:boolean;
+}
+
+const allUsers: User4[] =[]
+
+allUsers.push({
+  name:"",
+  isActive:true
+})
+
+const MLmodes: number[][] = [
+  [2,3,4],
+  [3,6,7]
+]
