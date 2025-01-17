9e4d2faf7c208dce52cad38d78e1ad25941579db
SujalGeek <morwanisujal875@gmail.com>
2025-01-17 17:37:54 +0530

    done with enums

---
+++ arrays/enums.js
@@ -0,0 +1,8 @@
+var hcSeat = 10 /* SeatChoice.AISLE */;
+var SeatChoice3;
+(function (SeatChoice3) {
+    SeatChoice3["AISLE"] = "aisle";
+    SeatChoice3["MIDDLE"] = "middle";
+    SeatChoice3["WINDOW"] = "window";
+    SeatChoice3["CUSTOM"] = "custom";
+})(SeatChoice3 || (SeatChoice3 = {}));
--- arrays/enums.ts
+++ arrays/enums.ts
@@ -1,1 +1,20 @@
 // enum
+const enum SeatChoice {
+  AISLE =10 ,
+  MIDDLE = 22,
+  WINDOW
+}
+
+const hcSeat = SeatChoice.AISLE;
+
+const enum SeatChoice2 {
+  AISLE ="aisle" ,
+  MIDDLE = 22,
+  WINDOW
+}
+enum SeatChoice3 {
+  AISLE ="aisle" ,
+  MIDDLE = "middle",
+  WINDOW = "window",
+  CUSTOM = "custom"
+}
